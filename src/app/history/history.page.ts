b038d360ed64c742cdb6d990767f6e2e8c907771
Jingting Chen <jingtingchen@Jingtings-MBP.zyxel.com>
2025-01-16 09:32:59 -0600

    Add history page ui

--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -7,5 +7,11 @@ import { Component } from "@angular/core";
   standalone: false,
 })
 export class HistoryPage {
+  items = [
+    ["Date", "Weight", "BMI", "Body Fat", "Status"],
+    ["Jan 14, 2025", "73.6kg", "20.0", "23.50%", "Normal"],
+    ["Jan 11, 2025", "74.4kg", "21.1", "22.30%", "Normal"],
+    ["Jan 10, 2025", "75.5kg", "22.0", "21.41%", "Normal"],
+  ];
   constructor() {}
 }
