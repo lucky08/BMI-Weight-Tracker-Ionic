4977975bfddec127f177ac76e085d84f3525d513
Jingting Chen <jingtingchen@Jingtings-MBP.zyxel.com>
2025-02-25 11:03:44 -0600

    Add demo uuid and device info

--- src/app/core/services/device.service.ts
+++ src/app/core/services/device.service.ts
@@ -23,13 +23,34 @@ export class DeviceService {
     return this.http.post<ModelDevice>(`${BACKEND_URL}/device`, device);
   }
 
-  async getDeviceId() {
+  /*async getDeviceId() {
     const info = await CapacitorDevice.getId();
     return info.identifier;
   }
 
   async getDeviceInfo() {
     const info = await CapacitorDevice.getInfo();
+    return info;
+  } */
+
+  // Add demo uuid
+  async getDeviceId() {
+    const info = { identifier: 'f47ac10b-58cc-4372-a567-0e02b2c3d479' };
+    return info.identifier;
+  }
+
+  // Add demo device info
+  async getDeviceInfo() {
+    const info = {
+      uuid: 'f47ac10b-58cc-4372-a567-0e02b2c3d479',
+      isVirtual: false,
+      manufacturer: 'Google Inc.',
+      model: 'Macintosh',
+      operatingSystem: 'mac',
+      osVersion: '11.16.8',
+      platform: 'web',
+      webViewVersion: '135.1.2.0',
+    };
     return info;
   }
 }
