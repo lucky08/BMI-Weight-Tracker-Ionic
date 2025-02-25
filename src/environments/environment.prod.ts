70ad195c2136dede5d81727dac255c11777d8a9b
Jingting Chen <jingtingchen@Jingtings-MBP.zyxel.com>
2025-02-25 10:09:00 -0600

    Update buildspec.yml and template.yaml

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://tx3dvax3cd.execute-api.us-east-2.amazonaws.com/prod',
 };
---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://tx3dvax3cd.execute-api.us-east-2.amazonaws.com/Stage',
+};
